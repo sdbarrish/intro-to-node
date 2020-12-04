f575d10fb5308f18e40399a046d3e80dd23ab8d8
Your Namesdbarrish <you@youraddress.comscott@danaandscott.com>
2020-12-04 12:21:24 -0500

    updated package.json, procfile, and hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
 
@@ -21,6 +22,6 @@ http.createServer(function (req, res) {
 		return res.end();
 	})
 
-}).listen(8080);
+}).listen(PORT);
 
 console.log("Server is listening on port 8080...")
